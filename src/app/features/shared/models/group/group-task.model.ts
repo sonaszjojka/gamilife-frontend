48eab529d005c6fba1b15d44190731423cd88d90
WroJan25 <s28044@pjwstk.edu.pl>
2025-11-30 00:40:24 +0100

    feature: init task function for groups, add group task model

---
+++ src/app/features/shared/models/group/group-task.model.ts
@@ -0,0 +1,30 @@
+export interface GroupTaskModel
+{
+groupTaskId: string;
+reward:number;
+acceptedDate:string;
+taskDto: TaskDto;
+groupTaskMembers: GroupTaskMemberModel[];
+
+}
+
+
+export interface TaskDto
+{
+  id:string;
+  title:string;
+  startTime:string;
+  endTime:string;
+  category:number;
+  difficulty:number;
+  completedAt:string|null;
+  description:string|null;
+}
+
+export interface GroupTaskMemberModel{
+  groupTaskMemberId:string;
+  groupMemberId:string;
+  isMarkedAsDone:boolean;
+}
+
+
