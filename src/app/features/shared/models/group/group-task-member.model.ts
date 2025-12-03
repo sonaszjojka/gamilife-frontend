bc878c5658f865d2dd39e3592500007f53ab5000
WroJan25 <s28044@pjwstk.edu.pl>
2025-12-03 17:49:25 +0100

    refactor: separate file for models associated with group task member

---
+++ src/app/features/shared/models/group/group-task-member.model.ts
@@ -0,0 +1,16 @@
+
+export interface GroupTaskMemberModel{
+  groupTaskMemberId:string;
+  groupMemberId:string;
+  isMarkedDone:boolean;
+}
+
+export interface EditGroupTaskMemberDto {
+  isMarkedDone: boolean;
+}
+
+export interface CreateGroupTaskMemberDto {
+  groupMemberId: string;
+  groupTaskId:string;
+  isMarkedDone:boolean;
+}
