b2964c6a6689873b70be97ba42a1debf05720c1e
WroJan25 <s28044@pjwstk.edu.pl>
2025-12-10 13:55:56 +0100

    feat: add store models

---
+++ src/app/features/shared/models/store/store.model.ts
@@ -0,0 +1,52 @@
+export interface StoreItemDto
+{
+  id:string;
+  name:string;
+  imagePath:string;
+  itemSlot:ItemSlotDto;
+  rarity:RarityDto;
+  price:number;
+
+}
+
+export interface StoreItemDetailsDto
+{
+  id:string;
+  name:string;
+  imagePath:string;
+  itemSlot:ItemSlotDto;
+  rarity:RarityDto;
+  price:number;
+  description:string;
+  isAlreadyOwned:boolean;
+
+}
+
+export interface ItemSlotDto
+{
+  id:number;
+  name:string;
+}
+
+export interface RarityDto
+{
+  id:number;
+  name:string;
+}
+
+export interface PurchaseStoreItemResult
+{
+  userInventoryItemId:string;
+  itemId:string;
+  quantity:number;
+  newUserMoney:number;
+
+}
+export interface StoreFiltersModel
+{
+  itemName?:string;
+  itemSlot?:number;
+  rarity?:number;
+  size: number;
+  page: number;
+}
