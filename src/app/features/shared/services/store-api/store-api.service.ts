b5d6ad0f3a3957ae2794a9d403081f66a4497211
WroJan25 <s28044@pjwstk.edu.pl>
2025-12-10 13:57:46 +0100

    feat: add store api service

---
+++ src/app/features/shared/services/store-api/store-api.service.ts
@@ -0,0 +1,79 @@
+import {inject, Injectable} from '@angular/core';
+import {environment} from '../../../../../environments/environment';
+import {HttpClient, HttpParams} from '@angular/common/http';
+import {
+  ItemSlotDto,
+  PurchaseStoreItemResult,
+  RarityDto,
+  StoreFiltersModel,
+  StoreItemDto
+} from '../../models/store/store.model';
+import {Observable} from 'rxjs';
+import {Page} from '../tasks/individual-task.service';
+
+@Injectable({
+  providedIn:"root"
+})
+export class StoreApiService
+{
+  private API_URL = `${environment.apiUrl}`;
+  private http = inject(HttpClient);
+
+
+  public getFilteredItems(filters:StoreFiltersModel) :Observable<Page<StoreItemDto>>
+  {
+    const params = this.buildHttpParamsForGetItems(filters)
+
+    return this.http.get<Page<StoreItemDto>>(`${this.API_URL}/store`,
+      {
+        params:params,
+        withCredentials:true
+      })
+  }
+
+  public getItemSlots():Observable<ItemSlotDto[]>
+  {
+    return this.http.get<ItemSlotDto[]>(`${this.API_URL}/item-slots`,
+      {
+        withCredentials:true
+      })
+  }
+  public getRarities():Observable<RarityDto[]>
+  {
+    return this.http.get<RarityDto[]>(`${this.API_URL}/item-rarities`,
+      {
+        withCredentials:true
+      })
+  }
+
+  public purchaseItem(itemId:string):Observable<PurchaseStoreItemResult>
+  {
+    const userId= localStorage.getItem('userId')
+    return this.http.post<PurchaseStoreItemResult>(`${this.API_URL}/users/${userId}/inventory/items`,itemId,{
+      withCredentials:true
+    } )
+
+  }
+
+  protected buildHttpParamsForGetItems(params: StoreFiltersModel) {
+    let httpParams = new HttpParams();
+
+    if (params.itemSlot!==undefined) {
+      httpParams = httpParams.set('itemSlot', params.itemSlot);
+    }
+    if (params.rarity !== undefined) {
+      httpParams = httpParams.set('rarity', params.rarity);
+    }
+    if (params.itemName) {
+      httpParams = httpParams.set('itemName', params.itemName);
+    }
+    if (params.page !== undefined) {
+      httpParams = httpParams.set('page', params.page.toString());
+    }
+    if (params.size !== undefined) {
+      httpParams = httpParams.set('size', params.size.toString());
+    }
+    return httpParams;
+  }
+
+}
