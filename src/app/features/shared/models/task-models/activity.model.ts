c6c6a14771330d583bcf08168626bbefb1899bf5
WroJan25 <s28044@pjwstk.edu.pl>
2025-12-16 20:08:56 +0100

    refactor: add activity model for get all users activities

---
+++ src/app/features/shared/models/task-models/activity.model.ts
@@ -0,0 +1,51 @@
+
+
+export interface ActivityItemDetails
+{
+  id: string,
+  type:ActivityType,
+  title:string,
+  description:string,
+  userId:string,
+  categoryId:number,
+  categoryName:string,
+  difficultyId:number,
+  difficultyName:string,
+  deadlineDate:string,
+  deadlineTime:string,
+  status:ActivityStatus,
+  pomodoro?:Pomodoro
+
+}
+
+export enum ActivityType {
+  TASK = 'TASK',
+  HABIT = 'HABIT',
+}
+
+export enum ActivityStatus {
+  ALIVE = 'ALIVE',
+  INCOMPLETE = 'INCOMPLETE',
+  DEADLINE_TODAY = 'DEADLINE_TODAY',
+  DEADLINE_MISSED = 'DEADLINE_MISSED',
+}
+
+export interface Pomodoro
+{
+  id : string,
+  cyclesRequired:number,
+  cyclesCompleted:number
+}
+
+export interface  GetAllActivitiesParams
+{
+  page:number;
+  size:number;
+  title:string|undefined;
+  categoryId:number|undefined;
+  difficultyId:number|undefined;
+  startDate:string|undefined;
+  endDate:string|undefined;
+  [key: string]: string | number|undefined;
+}
+
