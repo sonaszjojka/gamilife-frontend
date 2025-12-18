c628ca75848ce6b2741233720d4b7e6dbf35dae1
WroJan25 <s28044@pjwstk.edu.pl>
2025-12-18 16:45:52 +0100

    refactor: add reset date filter in task calendar component

--- src/app/features/private/shared/components/tasks/task-calendar/task-calendar.component.ts
+++ src/app/features/private/shared/components/tasks/task-calendar/task-calendar.component.ts
@@ -1,9 +1,10 @@
-import {Component, input, Input, output} from '@angular/core';
+import {Component, inject, input, Input, OnInit, output, signal} from '@angular/core';
 import { NzBadgeModule } from 'ng-zorro-antd/badge';
 import { NzCalendarModule } from 'ng-zorro-antd/calendar';
 import { NzButtonComponent } from 'ng-zorro-antd/button';
 import { NzIconDirective } from 'ng-zorro-antd/icon';
 import {ActivityItemDetails} from '../../../../../shared/models/task-models/activity.model';
+import {IndividualTaskService} from '../../../../../shared/services/tasks/individual-task.service';
 
 @Component({
   selector: 'app-task-calendar',
@@ -17,9 +18,20 @@ import {ActivityItemDetails} from '../../../../../shared/models/task-models/acti
   standalone: true,
   styleUrl: './task-calendar.component.css',
 })
-export class TaskCalendarComponent {
- taskCalendarList=input.required<ActivityItemDetails[]>();
- deadlineSelected= output<string>()
+export class TaskCalendarComponent implements OnInit{
+ taskCalendarList:ActivityItemDetails[]=[] ;
+ deadlineSelected= output<string|null>();
+ currentlySelectedDate=signal<Date|null>(null);
+ private taskService = inject(IndividualTaskService)
+
+ngOnInit() {
+
+   this.taskService.getAllActivities(0,1000,null, null,null,null,null).subscribe({
+     next:(response)=> {this.taskCalendarList=response.content},
+     error:(err)=>console.error(err)
+     }
+   )
+}
 
   isSameDay(activityDeadline: string | Date, calendarDate: Date): boolean {
     const activityDate = new Date(activityDeadline);
@@ -33,6 +45,15 @@ export class TaskCalendarComponent {
 
 
   onDateSelect($event: Date) {
-   this.deadlineSelected.emit($event.toISOString().slice(0,10))
+   if ($event.toISOString()==this.currentlySelectedDate()?.toISOString())
+   {
+     this.deadlineSelected.emit(null)
+     this.currentlySelectedDate.set(null)
+   }
+   else {
+     this.currentlySelectedDate.set($event)
+     this.deadlineSelected.emit($event.toISOString().slice(0, 10))
+   }
+
   }
 }
