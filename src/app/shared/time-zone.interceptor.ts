b7711ac2a02a9484401f051ae7e94a27592000c0
WroJan25 <s28044@pjwstk.edu.pl>
2025-12-26 00:01:33 +0100

    feature: add timezone interceptor

---
+++ src/app/shared/time-zone.interceptor.ts
@@ -0,0 +1,13 @@
+import { HttpInterceptorFn } from '@angular/common/http';
+
+export const timeZoneInterceptor: HttpInterceptorFn = (req, next) => {
+
+  const timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+  const newReq = req.clone({
+    setHeaders: {
+      'X-Timezone': timeZone
+    }
+  });
+
+  return next(newReq);
+};
