197ff73f48dadc39d0b78c4478d7c13e48b872fa
s27523 <s27523@pjwstk.edu.pl>
2025-12-28 17:52:23 +0100

    fix: fix frontend config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,10 +2,10 @@ const callbackUri = 'oauth2/callback';
 
 export const environment = {
   production: true,
-  apiUrl: 'https://api.gamilife.pl',
+  apiUrl: 'https://gamilife-backend.onrender.com/api/v1',
   googleClientId:
     '886417696654-ocac0ev9b6b00kcs5gn1gakeg3ujhg5p.apps.googleusercontent.com',
   callbackUri,
-  googleRedirectUri: 'https://gamilife.pl/oauth2/callback',
-  wsUrl: 'wss://gamilife.pl/ws/websocket',
+  googleRedirectUri: 'https://gamilife.onrender.com/oauth2/callback',
+  wsUrl: 'wss://gamilife.onrender.com/ws/websocket',
 };
