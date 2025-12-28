ba74afde0820c174eccb0f5c78bfd06c831c61fe
WroJan25 <s28044@pjwstk.edu.pl>
2025-12-28 16:32:51 +0100

    feature: add group shop module services

---
+++ src/app/features/shared/services/group-member-inventory-api/group-member-inventory-api.service.ts
@@ -0,0 +1,46 @@
+import {inject, Injectable} from '@angular/core';
+import {HttpClient, HttpParams} from '@angular/common/http';
+import {environment} from '../../../../../environments/environment';
+import {Observable} from 'rxjs';
+import {OwnedGroupItemResponseModel} from '../../models/group/owned-group-item.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GroupMemberInventoryApiService {
+
+  private http = inject(HttpClient);
+  private apiUrl = environment.apiUrl;
+
+  getMemberInventory(groupId: string, memberId: string,isUsedUp:boolean|null,page:number,size:number) {
+    let httpParams = new HttpParams();
+    httpParams = httpParams.set('page', page.toString());
+    httpParams = httpParams.set('size', size.toString());
+    if (isUsedUp !== null) {
+      httpParams = httpParams.set('isUsedUp', isUsedUp);
+    }
+    return this.http.get(
+      `${this.apiUrl}/groups/${groupId}/members/${memberId}/inventory`,
+      { params: httpParams
+        ,withCredentials: true },
+    );
+
+  }
+
+  deleteMemberInventoryItem(groupId: string, memberId: string, itemId: string) {
+    return this.http.delete(
+      `${this.apiUrl}/groups/${groupId}/members/${memberId}/inventory/items/${itemId}`,
+      { withCredentials: true },
+    );
+
+  }
+
+  useMemberItem(groupId: string, memberId: string, itemId: string, isUsedUp: boolean):Observable<OwnedGroupItemResponseModel> {
+   return this.http.put<OwnedGroupItemResponseModel>(
+      `${this.apiUrl}/groups/${groupId}/members/${memberId}/inventory/items/${itemId}/use`,
+      { isUsedUp },
+      { withCredentials: true },
+    );
+  }
+
+}
