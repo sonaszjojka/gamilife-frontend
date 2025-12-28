42b575e810fbeedb6716a3481ffa54cabc7c1830
s27523 <s27523@pjwstk.edu.pl>
2025-12-29 00:03:09 +0100

    fix: revert prev changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,10 +2,10 @@ const callbackUri = 'oauth2/callback';
 
 export const environment = {
   production: true,
-  apiUrl: 'https://localhost:8080/api/v1',
+  apiUrl: 'https://gamilife-backend.onrender.com/api/v1',
   googleClientId:
     '886417696654-ocac0ev9b6b00kcs5gn1gakeg3ujhg5p.apps.googleusercontent.com',
   callbackUri,
   googleRedirectUri: 'https://gamilife.pl/oauth2/callback',
-  wsUrl: 'wss://localhost:8080/ws/websocket',
+  wsUrl: 'wss://gamilife-backend.onrender.com/ws',
 };
