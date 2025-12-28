9bbb6bb5eac9ff9ce894acca883e5c712576c71c
s27523 <s27523@pjwstk.edu.pl>
2025-12-28 17:08:40 +0100

    fix: fix api url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ const callbackUri = 'oauth2/callback';
 
 export const environment = {
   production: true,
-  apiUrl: 'api.gamilife.pl',
+  apiUrl: 'https://api.gamilife.pl',
   googleClientId:
     '886417696654-ocac0ev9b6b00kcs5gn1gakeg3ujhg5p.apps.googleusercontent.com',
   callbackUri,
