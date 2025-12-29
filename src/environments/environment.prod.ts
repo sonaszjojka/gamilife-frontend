a9427fcc63c2e58937a608ba4b8d840eac6a6f51
s27523 <s27523@pjwstk.edu.pl>
2025-12-29 23:17:46 +0100

    hotfix: fix ws url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -7,5 +7,5 @@ export const environment = {
     '886417696654-ocac0ev9b6b00kcs5gn1gakeg3ujhg5p.apps.googleusercontent.com',
   callbackUri,
   googleRedirectUri: 'https://gamilife.pl/oauth2/callback',
-  wsUrl: 'wss://api.gamilife.pl/ws',
+  wsUrl: 'wss://api.gamilife.pl/ws/websocket',
 };
