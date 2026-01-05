128278e5d978a5d7ef5d0df09776b45d53e0e7b2
Krzysztof Drapała <krzysztof.drapson@gmail.com>
2026-01-05 20:53:54 +0100

    feat: improve group inv handling

--- src/app/features/shared/models/group/group-invitation.model.ts
+++ src/app/features/shared/models/group/group-invitation.model.ts
@@ -3,8 +3,25 @@ export interface EditGroupInvitationStatusDto {
   token: string;
 }
 
-export enum InvitationStatus {
-  PENDING = 1,
+export enum InvitationStatusId {
+  SENT = 1,
   ACCEPTED = 2,
   REJECTED = 3,
+  REVOKED = 4,
+}
+
+export enum InvitationStatus {
+  SENT = 'SENT',
+  ACCEPTED = 'ACCEPTED',
+  REJECTED = 'REJECTED',
+  REVOKED = 'REVOKED',
+}
+
+export interface GroupInvitation {
+  id: string;
+  groupId: string;
+  groupName: string;
+  invitedUserId: string;
+  status: InvitationStatus;
+  expiresAt: number;
 }
--- src/app/features/shared/services/group-invitation-api/group-invitation-api.service.ts
+++ src/app/features/shared/services/group-invitation-api/group-invitation-api.service.ts
@@ -3,14 +3,23 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from '../../../../../environments/environment';
 import { CreateGroupInvitationDto } from '../../models/group/user.model';
-import { EditGroupInvitationStatusDto } from '../../models/group/group-invitation.model';
+import {
+  EditGroupInvitationStatusDto,
+  GroupInvitation,
+} from '../../models/group/group-invitation.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class GroupInvitationApiService {
-  private http = inject(HttpClient);
-  private apiUrl = environment.apiUrl;
+  private readonly http = inject(HttpClient);
+  private readonly apiUrl = environment.apiUrl;
+
+  getById(groupId: string, invitationId: string): Observable<GroupInvitation> {
+    return this.http.get<GroupInvitation>(
+      `${this.apiUrl}/groups/${groupId}/invitations/${invitationId}`,
+    );
+  }
 
   createInvitation(
     groupId: string,
