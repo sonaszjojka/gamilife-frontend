08418ece06cce29ff92b80b6b1dcc5d0fbcd620b
Krzysztof Drapała <krzysztof.drapson@gmail.com>
2026-01-06 02:46:08 +0100

    fix: further improve dashboard appearance

--- src/app/features/private/dashboard/components/user-statistics-card/user-statistics-card.component.ts
+++ src/app/features/private/dashboard/components/user-statistics-card/user-statistics-card.component.ts
@@ -1,7 +1,6 @@
 import { Component, input } from '@angular/core';
 import { UserStatisticsModel } from '../../../../shared/models/user-profile/user-statistics.model';
 import { NzCardComponent } from 'ng-zorro-antd/card';
-import { NzDividerComponent } from 'ng-zorro-antd/divider';
 import { NzSpinComponent } from 'ng-zorro-antd/spin';
 import { NzIconDirective } from 'ng-zorro-antd/icon';
 
@@ -10,12 +9,7 @@ import { NzIconDirective } from 'ng-zorro-antd/icon';
   standalone: true,
   templateUrl: 'user-statistics-card.component.html',
   styleUrl: 'user-statistics-card.component.css',
-  imports: [
-    NzCardComponent,
-    NzDividerComponent,
-    NzSpinComponent,
-    NzIconDirective,
-  ],
+  imports: [NzCardComponent, NzSpinComponent, NzIconDirective],
 })
 export class UserStatisticsCardComponent {
   statistics = input<UserStatisticsModel[]>([]);
